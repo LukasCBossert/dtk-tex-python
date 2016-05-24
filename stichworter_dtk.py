99d01b526c1abcc13f938a05cf08057d28d70bc2
Lukas C. Bossert <lukas@digitales-altertum.de>
2016-05-24 12:05:30 +0200

    #1 #2

---
+++ stichworter_dtk.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import re
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+
+class Stichworter():
+    
+    def __init__(self):
+        print('"Stichworter" by Uwe Ziegenhagen, ziegenhagen@gmail.com')
+
+    def regReplace(self,file_path, pattern, subst):
+        """
+        Based on http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python 
+        this method uses regular expressions for the matching.
+        """
+        #Create temporary file
+        fh, abs_path = mkstemp()
+        with open(abs_path,'w') as new_file:
+            with open(file_path) as old_file:
+                for line in old_file:
+                    #print('>',line)
+                    new_file.write(re.sub(pattern,subst,line))
+        close(fh)
+        #Remove original file
+        remove(file_path)
+        #Move new file
+        move(abs_path, file_path)
+
+    def process(self,jobname):
+        # read file with Keyword:Page format
+        df = pd.read_csv(jobname+'.sti',sep=':',names=['Stichwort','Seite'])
+        # Create unique set of pages for the Keywords
+        # http://stackoverflow.com/questions/35964689/creating-a-sorted-list-of-unique-numbers-from-a-dataframe/35964970#35964970
+        result = df.groupby(['Stichwort'], as_index=False)['Seite'].agg(lambda x: ', '.join(np.unique(x).astype(str)))
+        result = pd.DataFrame(result) # create dataframe from the resulting series
+        result = result.set_index('Stichwort') # Set the index to the Stichwort column, since we are to use this as key
+
+        # export list of all keywords to LaTeX file
+        result.to_latex(jobname+'.tab',index=False)
+        
+        # Go through the list of Keyword/Pages Tupels
+        # and search for each Keyword. Replace the content of the pages {<content>}
+        # with the joint list of pages
+        for index, row in result.iterrows():
+            suchmuster = re.compile('(\\\\ausgabe{'+ index + '}{)(.*)(})')
+            self.regReplace(jobname+'.tex',suchmuster,'\\\\ausgabe{'+ index +'}{' + row[0] + '}')
+            print(index,':',row[0])
+            
+# put here the name of the TeX-file
+# more than one TeX file is currently not supported or at least not tested
+x = Stichworter().process('dtk_catalogentry')
